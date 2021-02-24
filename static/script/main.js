4e8cffe4670b0556a96d3b65304b2f40bbdac553
amesteDev <info@ameste.se>
2021-02-24 23:00:08 +0100

    login script frontend

---
+++ static/script/main.js
@@ -0,0 +1,31 @@
+const getEl = (id) => {
+    return document.getElementById(id);
+}
+
+let loginForm = getEl('loginForm');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    userdata = {
+        username: getEl('username').value,
+        password: getEl('password').value
+    };
+
+    fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userdata)
+        })
+        .then(response => response.json())
+        .then(data => {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('username', data.username);
+            window.location.replace('/signedin.html');
+        })
+        .catch(err => {
+            // VISA FELSIDA HÄR
+            console.log(err);
+        });
+})
