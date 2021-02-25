9a1422c12ee5d50f62c2317cec42cec99ca6945b
amesteDev <info@ameste.se>
2021-02-25 13:53:17 +0100

    rework of login

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const userModel = require("../models/userModel");
+const User = require("../models/userModel");
 const jwt = require('jsonwebtoken');
 
 router.post('/', async(req, res) => {
-    let user = await userModel.findOne({ username: req.body.username });
-
-    if (!user) {
-        return res.status(400).send('No user found or bad password');
-    }
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-    if (!validPassword) {
-        return res.status(400).send('No user found or bad password');
-    }
-
-    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.PK);
-    let username = user.username;
-    let userLevel = user.userLevel;
-
-    res.status(200).send({ token, username });
+    User.findOne({ username: req.body.username })
+        .exec((err, user) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
+            const validPassword = bcrypt.compare(req.body.password, user.password);
+            if (!validPassword) {
+                return res.status(401).send({
+                    token: null,
+                    message: "Invalid Password!"
+                });
+            }
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.PK, { expiresIn: 86400 });
+            res.status(200).send({
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                userLevel: user.userLevel,
+                token
+            });
+        });
 })
 
 module.exports = router;
